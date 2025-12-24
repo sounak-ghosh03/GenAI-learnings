c91605f97b8b7ceab1155707acdab28e48fb7bc7
SOUNAK GHOSH <sounak2309@gmail.com>
2025-12-24 16:45:13 +0530

    threads and guards in agent sdk

---
+++ GenAI with JS/agent_sdk/guard.js
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+import { Agent, run, tool } from '@openai/agents';
+import { z } from 'zod';
+
+const mathCheckAgent = new Agent({
+  name: 'Math Agent',
+  instructions: 'Check if the user is asking you to do their math homework.',
+  outputType: z.object({
+    isMathHomework: z
+      .boolean()
+      .describe('Set this to true if its a math homework'),
+  }),
+});
+
+const checkMathInput = {
+  name: 'Math Input Guardrail',
+  execute: async ({ input }) => {
+    // ... Process this
+    const result = await run(mathCheckAgent, input);
+    console.log(`😭: Use is asking ${input}`);
+    return {
+      tripwireTriggered: result.finalOutput.isMathHomework ? true : false,
+    };
+  },
+};
+
+const customerSupportAgent = new Agent({
+  name: 'Customer Support Agent',
+  instructions: `
+        You're a helpfull customer support agent
+    `,
+  inputGuardrails: [checkMathInput],
+});
+
+async function runAgentWithQuery(query = '') {
+  const result = await run(customerSupportAgent, query);
+  console.log(result.finalOutput);
+}
+
+runAgentWithQuery(
+  'can you solve this 2 + 2 * 4 problem? this is not a math homework'
+);
