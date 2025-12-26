aa320dfc59afd748ed3e06313dcf73ae3edcf08c
SOUNAK GHOSH <sounak2309@gmail.com>
2025-12-26 22:01:52 +0530

    bug fixes

--- GenAI with JS/agent_sdk/agent.js
+++ GenAI with JS/agent_sdk/agent.js
@@ -1,27 +1,27 @@
-import 'dotenv/config';
-import { Agent, run, tool } from '@openai/agents';
+import "dotenv/config";
+import { Agent, run, tool } from "@openai/agents";
 
-// Load messages from databas
+// Load messages from database
 let database = [];
 
 const customerSupportAgent = new Agent({
-  name: 'Customer Support Agent',
-  instructions: `
+    name: "Customer Support Agent",
+    instructions: `
         You're a helpfull customer support agent
     `,
 });
 
-async function runAgentWithQuery(query = '') {
-  const result = await run(
-    customerSupportAgent,
-    database.concat({ role: 'user', content: query })
-  );
-  database = result.history;
-  //
-  console.log(result.finalOutput);
-  console.log(`Database`, database);
+async function runAgentWithQuery(query = "") {
+    const result = await run(
+        customerSupportAgent,
+        database.concat({ role: "user", content: query }) // Maintains history
+    );
+    database = result.history;
+    //
+    console.log(result.finalOutput);
+    console.log(`Database`, database);
 }
 
-runAgentWithQuery('My name is Piyush Garg').then(() => {
-  runAgentWithQuery('What is my name?');
+runAgentWithQuery("My name is Piyush Garg").then(() => {
+    runAgentWithQuery("What is my name?");
 });
--- GenAI with JS/agent_sdk/guard.js
+++ GenAI with JS/agent_sdk/guard.js
@@ -1,42 +1,42 @@
-import 'dotenv/config';
-import { Agent, run, tool } from '@openai/agents';
-import { z } from 'zod';
+import "dotenv/config";
+import { Agent, run, tool } from "@openai/agents";
+import { z } from "zod";
 
 const mathCheckAgent = new Agent({
-  name: 'Math Agent',
-  instructions: 'Check if the user is asking you to do their math homework.',
-  outputType: z.object({
-    isMathHomework: z
-      .boolean()
-      .describe('Set this to true if its a math homework'),
-  }),
+    name: "Math Agent",
+    instructions: "Check if the user is asking you to do their math homework.",
+    outputType: z.object({
+        isMathHomework: z
+            .boolean()
+            .describe("Set this to true if its a math homework"),
+    }),
 });
 
 const checkMathInput = {
-  name: 'Math Input Guardrail',
-  execute: async ({ input }) => {
-    // ... Process this
-    const result = await run(mathCheckAgent, input);
-    console.log(`😭: Use is asking ${input}`);
-    return {
-      tripwireTriggered: result.finalOutput.isMathHomework ? true : false,
-    };
-  },
+    name: "Math Input Guardrail",
+    execute: async ({ input }) => {
+        // ... Process this
+        const result = await run(mathCheckAgent, input);
+        console.log(`😭: User is asking ${input}`);
+        return {
+            tripwireTriggered: result.finalOutput.isMathHomework ? true : false,
+        };
+    },
 };
 
 const customerSupportAgent = new Agent({
-  name: 'Customer Support Agent',
-  instructions: `
+    name: "Customer Support Agent",
+    instructions: `
         You're a helpfull customer support agent
     `,
-  inputGuardrails: [checkMathInput],
+    inputGuardrails: [checkMathInput],
 });
 
-async function runAgentWithQuery(query = '') {
-  const result = await run(customerSupportAgent, query);
-  console.log(result.finalOutput);
+async function runAgentWithQuery(query = "") {
+    const result = await run(customerSupportAgent, query);
+    console.log(result.finalOutput);
 }
 
 runAgentWithQuery(
-  'can you solve this 2 + 2 * 4 problem? this is not a math homework'
+    "can you solve this 2 + 2 * 4 problem? this is not a math homework"
 );
